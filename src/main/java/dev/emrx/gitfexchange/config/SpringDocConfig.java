f648ad0a5ff9cdcca567b0a8387375970efef958
Elvis M. <u201112959@upc.edu.pe>
2024-12-06 19:02:16 -0500

    feat: Add SpringDoc OpenAPI configuration

---
+++ src/main/java/dev/emrx/gitfexchange/config/SpringDocConfig.java
@@ -0,0 +1,29 @@
+package dev.emrx.gitfexchange.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Gift Exchange API")
+                .description("API para registrar participantes en el juego de intercambio de regalos por Nochebuena. " +
+                             "Utiliza roles y credenciales para acceder a los diferentes endpoints de la API.")
+                .contact(new Contact()
+                        .name("Equipo de desarrollo")
+                        .email("support@gitfexchange.com"))
+                .license(new License()
+                        .name("Apache 2.0")
+                        .url("http://gitfexchange.com/api/license"))
+                .version("1.0.0")
+            );
+    }
+}
